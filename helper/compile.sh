6b62a8d58eb2dee2e9351e6243e0e0c593368d69
a1308076 <a1308076@unet.univie.ac.at>
2017-10-02 02:11:38 +0200

    Add instructions to compile script.

--- helper/compile.sh
+++ helper/compile.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-echo "compiling..."
-mkdir out
+if [[ ! -d out ]]; then
+  echo "Creating output directory..."
+  mkdir out
+fi
+
+echo "Compiling..."
 find ../contracts -name "*.sol" -exec solc --combined-json abi,bin {} + > out/combined.json
+echo "Done..."
+
+echo
+echo "--- Setup Instructions ---"
+
+echo "1: Open setup page and upload combined.json file."
+echo "2: Download resulting api.js and place it in ./out directory."
+echo "3: Run ./init.sh script."
