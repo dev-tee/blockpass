e92f6c98625b9c7d05462d0b753a50c753b63071
a1308076 <a1308076@unet.univie.ac.at>
2017-10-08 00:29:42 +0200

    Add instructions and file checks to compile script.

--- helper/compile.sh
+++ helper/compile.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-if [[ ! -d out ]]; then
+OUT="out"
+JSON="$OUT/combined.json"
+API="$OUT/api.js"
+
+if [[ ! -d $OUT ]]; then
   echo "Creating output directory..."
   mkdir out
+else
+  echo "Clearing output directory..."
+  if [[ -f $JSON ]]; then
+    rm $JSON
+  fi
+  if [[ -f $API ]]; then
+    rm $API
+  fi
 fi
 
 echo "Compiling..."
-find ../contracts -name "*.sol" -exec solc --combined-json abi,bin {} + > out/combined.json
+find ../contracts -name "*.sol" -exec solc --combined-json abi,bin {} + > $JSON
 echo "Done..."
 
 echo
@@ -15,3 +27,4 @@ echo "--- Setup Instructions ---"
 echo "1: Open setup page and upload combined.json file."
 echo "2: Download resulting api.js and place it in ./out directory."
 echo "3: Run ./init.sh script."
+echo "4: Open manager page and register all contracts."
